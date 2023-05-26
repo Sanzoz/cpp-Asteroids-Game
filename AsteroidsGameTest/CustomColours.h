ae4086ad33b1706db1300161f6b625391af4a338
Sanzoz <126629818+Sanzoz@users.noreply.github.com>
2023-05-26 19:00:58 +1000

    Initial commit

---
+++ AsteroidsGameTest/CustomColours.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <raylib.h>
+
+//Some custom colors made using RayLib's Color Class
+Color MALACHITE{ 100, 233, 134, 255 };
+Color DARKMALACHITE{ 70, 203, 104, 255 };
+Color DARKERMALACHITE{ 40, 173, 74, 255 };
+
+Color DARKERGRAY{ 34, 34, 34, 255 };
+Color KINDALIGHTGRAY{ 74, 74, 74, 255 };
+Color EVENLIGHTERGRAY{ 214, 214, 214, 255 };
+
+Color LIGHTORANGE{ 255, 166, 56, 255 };
+
+Color LIGHTRED{ 234, 75, 71, 255 };
+
+Color LIGHTYELLOW{ 255, 232, 0, 255 };
